f502d18c3f0c12948abe7885df4f3d4a7aca18a9
Harshitha KGS <kgsharshitha0@gmail.com>
2025-06-24 00:07:00 +0530

    Initial commit of Flask restaurant app

--- app.py
+++ app.py
@@ -57,4 +57,5 @@ def price():
     return render_template("price.html", prices=PRICES)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT via env var
+    app.run(debug=True, host='0.0.0.0', port=port)
